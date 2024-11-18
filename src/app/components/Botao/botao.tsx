811b458067d6d49da2079108a9bfc605a46c9a2e
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-18 20:38:03 -0300

    commit

---
+++ src/app/components/Botao/botao.tsx
@@ -0,0 +1,11 @@
+import styles from './Botao.module.css';
+
+const Botao = () => {
+    return (
+        <div className={styles.botao}>
+            <button>SAIBA MAIS!</button>
+        </div>
+    );
+};
+
+export default Botao;
---
+++ src/app/components/Menu/menu.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from 'next/link';
+
+export default function Menu() {
+    return (
+        <div className="menu">
+            <label className="home">
+                <Link href="/index">
+                    HOME
+                </Link>
+            </label>
+            <label className="comofun">
+                <Link href="/comofunciona">
+                    COMO FUNCIONA
+                </Link>
+            </label>
+
+            <img src="LOGONOME.png" alt="logo" width="200px" />
+
+            <label className="integrantes">
+                <Link href="/integrantes">
+                    INTEGRANTES
+                </Link>
+            </label>
+
+            <label className="fornecedores">
+                <Link href="/fornecedores">
+                    FORNECEDORES
+                </Link>
+            </label>
+        </div>
+    );
+}
