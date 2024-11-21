88e3255d89ec3ad74e1d8028830326bfa7b3a3e9
postos7 <luann.mariano2003@gmail.com>
2024-11-21 13:57:20 -0300

    Update page.tsx

--- src/app/Integrantes/page.tsx
+++ src/app/Integrantes/page.tsx
@@ -1,14 +1,16 @@
 import Menu from '../components/Menu/menu';
 import Henzo from '../components/Henzo/henzo';
 import Luann from '../components/Luann/luann';
+import Humberto from '../components/Humberto/humberto';
 
 export default function Integrantes() {
     return (
         <>
         <Menu />
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
-      <Henzo imageSrc="/HenzoFoto.jpg" title="Henzo Boschiero Puchetti RM555179" />
-      <Luann imageSrc="/luann2.jpg" title="Luann Domingos Mariano RM558548" />
+      <Henzo imageSrc="/HenzoFoto.jpg" title="Henzo Boschiero Puchetti - RM555179" />
+      <Luann imageSrc="/luann2.jpg" title="Luann Domingos Mariano - RM558548" />
+      <Humberto imageSrc="/luann2.jpg" title="Humberto - RM558548" />
       </div>
         </>
     )
