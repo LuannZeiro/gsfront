9828d5222f8a06082568ed20bdb1524d8c6ebefe
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 20:38:05 -0300

    commit

--- src/app/Integrantes/page.tsx
+++ src/app/Integrantes/page.tsx
@@ -2,12 +2,14 @@ import Menu from '../components/Menu/menu';
 import Henzo from '../components/Henzo/henzo';
 import Luann from '../components/Luann/luann';
 import Humberto from '../components/Humberto/humberto';
+import TituloIntegrantes from '../components/TituloIntegrantes/titulointegrantes';
 
 export default function Integrantes() {
     return (
         <>
         <Menu />
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
+        <TituloIntegrantes />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '400px' }}>
       <Henzo imageSrc="/HenzoFoto.jpg" title="Henzo Boschiero Puchetti - RM555179" />
       <Luann imageSrc="/luann2.jpg" title="Luann Domingos Mariano - RM558548" />
       <Humberto imageSrc="/HumbertoFoto.jpg" title="Humberto Souza - RM558482" />
