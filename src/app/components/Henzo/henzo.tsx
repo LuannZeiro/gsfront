fedef302fb95669c78587898a52f5ea614d7ec68
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 00:42:10 -0300

    commit

---
+++ src/app/components/Henzo/henzo.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  background-color: #ffffff;
+  border: 2px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+
+const Image = styled.img`
+  max-width: 90%;
+  max-height: 70%;
+  object-fit: cover;
+  border-radius: 5px;
+`;
+
+
+const Title = styled.h3`
+  margin-top: 10px;
+  font-size: 1.2rem;
+  color: #333;
+  text-align: center;
+`;
+
+type PhotoCardProps = {
+  imageSrc: string;
+  title: string;
+};
+
+const PhotoCard: React.FC<PhotoCardProps> = ({ imageSrc, title }) => {
+  return (
+    <Card>
+      <Image src={imageSrc} alt={title} />
+      <Title>{title}</Title>
+    </Card>
+  );
+};
+
+export default PhotoCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+import Botao from '../app/components/Botao/botao';
 import Menu from '../app/components/Menu/menu';
+import Texto from '../app/components/Texto/texto';
 import Titulo from '../app/components/Titulo/titulo';
-import Botao from './components/Botao/botao';
-import Texto from './components/Texto/texto';
+import Texto2 from '../app/components/Texto2/texto2';
+import Texto3 from '../app/components/Texto3/texto3';
+import Texto4 from '../app/components/Texto4/texto4';
 
 export default function Home() {
-  return (
-    <>
-      <Menu />
-      <Titulo />
-      <Texto />
-      <Botao />
-    </>
-  );
+    return (
+        <>
+            <Menu />
+            <Titulo />
+            <Texto />
+            <Texto2 />
+            <Texto3 />
+            <Texto4 />
+            <Botao />
+
+        </>
+    );
 }
+
