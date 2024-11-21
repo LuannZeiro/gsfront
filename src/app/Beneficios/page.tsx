848ede32b485197290a188c71077b574c6af42f8
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 20:19:46 -0300

    commit

--- src/app/Beneficios/page.tsx
+++ src/app/Beneficios/page.tsx
@@ -1,7 +1,11 @@
 import Menu from '../components/Menu/menu';
+import TituloBene from '../components/TituloBeneficios/titulobene';
 
 export default function ComoFunciona() {
     return (
+        <>
         <Menu />
+        <TituloBene />
+        </>
     )
 }
---
+++ src/app/components/TituloBeneficios/titulobene.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: #02282B;
+  text-align: center;
+  margin: 50px;
+`;
+
+const TituloBene = () => {
+    return (
+        <Title>
+            A energia limpa traz inúmeros benefícios para empresas como a Greenwatts, que atuam nesse setor. Alguns dos principais são:
+        </Title>
+    );
+};
+
+export default TituloBene;
