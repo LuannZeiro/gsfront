770c57110abd5e1721f02e6f979e5498826110a8
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 00:47:52 -0300

    commit

--- src/app/Integrantes/page.tsx
+++ src/app/Integrantes/page.tsx
@@ -1,7 +1,11 @@
+import Henzo from '../components/Henzo/henzo';
 import Menu from '../components/Menu/menu';
 
 export default function Integrantes() {
     return (
+        <>
         <Menu />
+        <Henzo />
+        </>
     )
 }
