b4a3cd99b7631d59c6bb281abecc348bcc348b92
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 21:02:49 -0300

    commit

--- src/app/Beneficios/page.tsx
+++ src/app/Beneficios/page.tsx
@@ -1,5 +1,6 @@
 import Beneficio1 from '../components/Beneficio1/beneficio1';
 import Beneficio2 from '../components/Beneficio2/beneficio2';
+import Beneficio3 from '../components/Beneficio3/beneficio3';
 import Menu from '../components/Menu/menu';
 import TituloBene from '../components/TituloBeneficios/titulobene';
 
@@ -10,6 +11,7 @@ export default function ComoFunciona() {
         <TituloBene />
         <Beneficio1 />
         <Beneficio2 />
+        <Beneficio3 />
         </>
     )
 }
--- src/app/components/Beneficio3/beneficio3.tsx
+++ src/app/components/Beneficio3/beneficio3.tsx
@@ -1,7 +1,7 @@
 export default function Beneficio3() {
     return (
         <p style={{ fontSize: '18px', lineHeight: '1.6', color: '#02282B' }}>
-           Redução de custos a longo prazo: Fontes renováveis, como solar e eólica, possuem menor custo operacional, tornando-se mais acessíveis com o passar do tempo.
+           Imagem positiva: Empresas como a Greenwatts são percebidas como inovadoras e comprometidas com a sustentabilidade, o que fortalece sua marca.
             </p>
     );
 }
