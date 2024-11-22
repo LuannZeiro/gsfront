310f0ea13c756ceaf0a6bed0f1a56e5ef9af1711
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 21:04:09 -0300

    adicionando beneficio 4

--- src/app/Beneficios/page.tsx
+++ src/app/Beneficios/page.tsx
@@ -1,6 +1,7 @@
 import Beneficio1 from '../components/Beneficio1/beneficio1';
 import Beneficio2 from '../components/Beneficio2/beneficio2';
 import Beneficio3 from '../components/Beneficio3/beneficio3';
+import Beneficio4 from '../components/Beneficio4/beneficio4';
 import Menu from '../components/Menu/menu';
 import TituloBene from '../components/TituloBeneficios/titulobene';
 
@@ -12,6 +13,7 @@ export default function ComoFunciona() {
         <Beneficio1 />
         <Beneficio2 />
         <Beneficio3 />
+        <Beneficio4 />
         </>
     )
 }
