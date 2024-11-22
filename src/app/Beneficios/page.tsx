00b1b9caefe193c5fa862c163b4d26e2d69ef0a5
Luann Mariano <luann.mariano2003@gmail.com>
2024-11-21 21:45:07 -0300

    commit

--- src/app/Beneficios/page.tsx
+++ src/app/Beneficios/page.tsx
@@ -3,6 +3,7 @@ import Beneficio2 from '../components/Beneficio2/beneficio2';
 import Beneficio3 from '../components/Beneficio3/beneficio3';
 import Beneficio4 from '../components/Beneficio4/beneficio4';
 import Beneficio5 from '../components/Beneficio5/beneficio5';
+import Beneficio6 from '../components/Beneficio6/beneficio6';
 import Menu from '../components/Menu/menu';
 import TituloBene from '../components/TituloBeneficios/titulobene';
 
@@ -16,6 +17,7 @@ export default function ComoFunciona() {
         <Beneficio3 />
         <Beneficio4 />
         <Beneficio5 />
+        <Beneficio6 />
         </>
     )
 }
